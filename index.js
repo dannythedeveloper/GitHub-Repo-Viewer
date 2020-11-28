163de58954a658e05d559b893e89400fb033b9d8
Danny Carcamo <dannycarcamo@icloud.com>
2020-11-28 14:50:05 -0600

    added error message

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const baseUrl = 'https://api.github.com';
 
 function displayResults(responseJson) {
-    console.log(responseJson);
+    console.log(responseJson);    
+    $('#error-message').empty();
     $('#results-list').empty();
-    for (let i=0; i < responseJson.length; i++) {
+    for (let i = 0; i < responseJson.length; i++) {
         $('#results-list').append(
-        `<li><h3>${responseJson[i].name}</h3>
+            `<li><h3>${responseJson[i].name}</h3>
         <p><a href="${responseJson[i].html_url}">Repo Link</a></p>
         </li>`);
     };
@@ -15,10 +16,18 @@ function displayResults(responseJson) {
 function getSearchResults(userInput) {
     const url = `${baseUrl}/users/${userInput}/repos`;
     console.log(url);
-    fetch(url) 
-    .then(response => response.json())
-    .then(responseJson => displayResults(responseJson));
-}
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(reponse.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => {
+            $('#error-message').text(`Something went wrong: ${error.message}`)
+        });
+};
 
 function watchForm() {
     $('form').on('submit', function (event) {
